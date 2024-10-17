7973f4da712126ad07ed6f260a7cdbdcae3028d3
Magomed Bekkaev <bekkaev.magomed@gmail.com>
2024-10-17 12:13:09 +0200

    exos fin

--- fonctions.php
+++ fonctions.php
@@ -123,3 +123,19 @@ function supprimerElement($listeCourses, $elementASupprimer) {
 $listeCourses = supprimerElement($listeCourses, "Lait");
 $listeCourses = supprimerElement($listeCourses, "Chocolat");
 afficherCourses($listeCourses);
+
+// Ajouter un élément qui n'est pas déjà dans la liste
+$listeCourses = ajouterElement($listeCourses, "Fromage");
+
+// Tenter d'ajouter un élément qui existe déjà
+$listeCourses = ajouterElement($listeCourses, "Pain");
+
+// Supprimer un élément qui existe dans la liste
+$listeCourses = supprimerElement($listeCourses, "Oeufs");
+
+// Tenter de supprimer un élément qui n'est pas dans la liste
+$listeCourses = supprimerElement($listeCourses, "Chocolat");
+
+// Afficher la liste après chaque action pour montrer les modifications
+echo "État final de la liste de courses :<br>";
+afficherCourses($listeCourses);
